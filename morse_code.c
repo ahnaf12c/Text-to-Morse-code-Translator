dfb528b461db024f34fbe56ff314b370d09ac5ac
Ahnaf101109 <148791555+ahnaf12c@users.noreply.github.com>
2024-01-26 22:30:00 +0600

    Update morse_code.c

--- morse_code.c
+++ morse_code.c
@@ -18,8 +18,8 @@ int main()
     while (1)
     {
         printf("Please enter a string (enter 'q' to exit, or 'Q' to see the morse code of Q): ");
-        scanf("%[^\n]");
-        //gets(string);
+        
+        gets(string);
         len = strlen(string);
 
         if (len == 1 && string[0] == 'q')
